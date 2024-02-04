e28a0b7abb35ce6ef6969c747c13e10b76d8c547
Ahmet Affan <ahmet.affan.ebcioglu@outlook.com>
2024-02-04 20:03:26 +0300

    menu

--- game/background.go
+++ game/background.go
@@ -1,43 +1,47 @@
 package game
 
 import (
+	"image/color"
+
 	"github.com/hajimehoshi/ebiten/v2"
 	assets "github.com/lidldev/GameResources"
 )
 
 type Background struct {
 	image *ebiten.Image
-
-	isDrawed bool
 }
 
 func (b *Background) ChangeScene(screen *ebiten.Image, c *camera, g *Game) {
 	b.image = assets.GopherWalkBackground
+	screen.Fill(color.Black)
 
-	if g.myBool {
-		screen.Fill(Blackish)
-	} else {
-		screen.Fill(LightBlue)
+	if g.menuOff {
+		if g.myBool {
+			screen.Fill(Blackish)
+		} else {
+			screen.Fill(LightBlue)
+		}
+
+		g.Door = assets.Door
+
+		op3 := &ebiten.DrawImageOptions{}
+		op3.GeoM.Scale(0.45, 0.35)
+		op3.GeoM.Translate(2000, 316)
+		g.camera.draw(g.Door, op3)
+
+		if g.myBool {
+			op := &ebiten.DrawImageOptions{}
+			op.GeoM.Scale(1, 0.79)
+			op.GeoM.Translate(-3000, 0)
+			c.draw(assets.GopherWalkBackground2, op)
+			b.image.Clear()
+			g.Door.Clear()
+		} else {
+			op := &ebiten.DrawImageOptions{}
+			op.GeoM.Scale(1, 0.8)
+			op.GeoM.Translate(-3000, 0)
+			c.draw(b.image, op)
+		}
 	}
 
-	g.Door = assets.Door
-
-	op3 := &ebiten.DrawImageOptions{}
-	op3.GeoM.Scale(0.45, 0.35)
-	op3.GeoM.Translate(2000, 316)
-	g.camera.draw(g.Door, op3)
-
-	if g.myBool {
-		op := &ebiten.DrawImageOptions{}
-		op.GeoM.Scale(1, 0.79)
-		op.GeoM.Translate(-3000, 0)
-		c.draw(assets.GopherWalkBackground2, op)
-		b.image.Clear()
-		g.Door.Clear()
-	} else {
-		op := &ebiten.DrawImageOptions{}
-		op.GeoM.Scale(1, 0.8)
-		op.GeoM.Translate(-3000, 0)
-		c.draw(b.image, op)
-	}
 }
--- game/game.go
+++ game/game.go
@@ -1,12 +1,17 @@
 package game
 
 import (
+	"bytes"
 	"fmt"
 	"image/color"
+	"log"
+	"time"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
+	"github.com/hajimehoshi/ebiten/v2/text/v2"
+	assets "github.com/lidldev/GameResources"
 )
 
 type Game struct {
@@ -15,8 +20,8 @@ type Game struct {
 
 	background Background
 	myBool     bool
-
-	Door *ebiten.Image
+	menuOff    bool
+	Door       *ebiten.Image
 }
 
 const (
@@ -30,61 +35,108 @@ var (
 	Blackish  = color.RGBA{0, 0, 50, 250}          //Basically night
 )
 
+var (
+	fontFaceSource *text.GoTextFaceSource
+)
+
+func init() {
+	s, err := text.NewGoTextFaceSource(bytes.NewReader(assets.Font_ttf))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fontFaceSource = s
+}
+
 func NewGame() *Game {
 	g := &Game{}
 	g.camera.init()
 	return g
 }
 
+const (
+	mytext  = "Gopher Walk"
+	mytext2 = "Press \"Enter\" to Start the Game"
+)
+
 func (g *Game) Draw(screen *ebiten.Image) {
-	g.camera.clear()
+	const (
+		normalFontSize = 24
+		bigFontSize    = 48
+	)
 
-	g.background.ChangeScene(screen, &g.camera, g)
+	const x = 175
 
-	g.player.Draw(screen, &g.camera)
+	op := &text.DrawOptions{}
+	op.GeoM.Translate(x, 60)
+	op.ColorScale.ScaleWithColor(color.White)
+	text.Draw(screen, mytext, &text.GoTextFace{
+		Source: fontFaceSource,
+		Size:   bigFontSize,
+	}, op)
 
-	g.camera.render(screen)
+	op = &text.DrawOptions{}
+	op.GeoM.Translate(140, 250)
+	op.ColorScale.ScaleWithColor(color.White)
+	text.Draw(screen, mytext2, &text.GoTextFace{
+		Source: fontFaceSource,
+		Size:   normalFontSize,
+	}, op)
 
-	msg := fmt.Sprintf("Gopher X: %.2f, Y: %.2f",
-		float64(g.player.player.x),
-		float64(g.player.player.y))
-	ebitenutil.DebugPrint(screen, msg)
+	if g.menuOff {
+		g.camera.clear()
 
-	msg2 := fmt.Sprintf("\nTPS: %.2f, FPS: %.2f, VSync: %v",
-		ebiten.ActualTPS(),
-		ebiten.ActualFPS(),
-		ebiten.IsVsyncEnabled())
-	ebitenutil.DebugPrint(screen, msg2)
+		g.background.ChangeScene(screen, &g.camera, g)
+
+		g.player.Draw(screen, &g.camera)
+
+		g.camera.render(screen)
+
+		msg := fmt.Sprintf("Gopher X: %.2f, Y: %.2f",
+			float64(g.player.player.x),
+			float64(g.player.player.y))
+		ebitenutil.DebugPrint(screen, msg)
+
+		msg2 := fmt.Sprintf("\nTPS: %.2f, FPS: %.2f, VSync: %v",
+			ebiten.ActualTPS(),
+			ebiten.ActualFPS(),
+			ebiten.IsVsyncEnabled())
+		ebitenutil.DebugPrint(screen, msg2)
+	}
 }
 
 func (g *Game) Update() error {
-	g.player.Update()
-	g.camera.setPos(g.player.player.x/unit-300, 0)
-
-	vsync := ebiten.IsVsyncEnabled()
-
-	if inpututil.IsKeyJustPressed(ebiten.KeyV) {
-		ebiten.SetVsyncEnabled(!vsync)
+	if inpututil.IsKeyJustPressed(ebiten.KeyEnter) {
+		g.menuOff = true
 	}
+	if g.menuOff {
+		g.player.Update()
+		g.camera.setPos(g.player.player.x/unit-300, 0)
 
-	if g.myBool {
+		vsync := ebiten.IsVsyncEnabled()
 
-	} else {
-		if g.player.player.x <= -27000 {
-			g.camera.setPos(-3000, 0)
+		if inpututil.IsKeyJustPressed(ebiten.KeyV) {
+			ebiten.SetVsyncEnabled(!vsync)
 		}
 
-		if g.player.player.x >= 26500 {
-			g.camera.setPos(2350, 0)
-		}
-	}
+		if g.myBool {
+		} else {
+			if g.player.player.x <= -27000 {
+				g.camera.setPos(-3000, 0)
+			}
 
-	if g.player.player.x >= 19900 && g.player.player.x <= 20500 {
-		if inpututil.IsKeyJustPressed(ebiten.KeyW) {
-			g.myBool = true
-			g.player.isBorder = true
-			g.background.isDrawed = false
-			g.player.player.x = 0
+			if g.player.player.x >= 26500 {
+				g.camera.setPos(2350, 0)
+			}
+		}
+
+		if g.player.player.x >= 19900 && g.player.player.x <= 20500 {
+			if inpututil.IsKeyJustPressed(ebiten.KeyW) {
+				g.myBool = true
+				g.player.isBorder = true
+				//g.background.menuOn = false
+				g.player.player.x = 0
+				time.Sleep(time.Second * 1)
+			}
 		}
 	}
 
